9af6782d2638ba661546bc7a67345e8b593b5387
phan hong hai <phanhonghai97@gmail.com>
2019-04-29 00:41:30 +0700

    up content 90%

--- public/js/function/carouselHome.js
+++ public/js/function/carouselHome.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
                     slidesToShow: 3,
                     slidesToScroll: 3,
                     infinite: true,
-                    dots: true
+                    dots: false
                 }
             },
             {
@@ -38,6 +38,42 @@ $(document).ready(function () {
             // You can unslick at a given breakpoint now by adding:
             // settings: "unslick"
             // instead of a settings object
+        ]
+    });
+    $('.pageCoDong .contentCoDong').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:false,
+        fade:true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 13,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
         ]
     });
 });
--- public/js/function/menu.js
+++ public/js/function/menu.js
@@ -1,9 +1,52 @@
 $(function(){
+    // menu responsive
     $('.menu-box').slideUp();
     $('.menu .tab-menu img').click(function(event){
         $('.menu-box').slideToggle(500);
     });
     $('.menu .tab-menu a').click(function(event){
         $('.menu-box').slideToggle(500);
+    });
+    // tab news
+    $('.nameTab button').click(function(){
+        let giaTriTab =$(this).index();
+        $('.nameTab .btn').removeClass('activeTab');
+        $('.newTab .imageNewTab .classifyNews').removeClass('activeClassify');
+        $(this).addClass('activeTab');
+        $('.contentTab .tab').removeClass('activeContentNew');
+        let vlaue=".contentTab .tab:nth-child("+(giaTriTab+1)+")";
+        let vlaue1=".newTab .imageNewTab .classifyNews:nth-child("+(giaTriTab+1)+")";
+        $(vlaue).addClass('activeContentNew');
+        $(vlaue1).addClass('activeClassify');
+    });
+    $('.contentTab .activeContentNew ul li').hover(function(){
+        let giaTriListItem=$(this).index();
+        $('.imageNewTab .newImage').removeClass('activeImageNew');
+        $(".imageNewTab .newImage:nth-child("+(giaTriListItem+1)+")").addClass('activeImageNew');
+        
+    });
+    $('.hoatDong .headCate .subCate ul li').click(function(){
+        let valueSubCate=$(this).index();
+        $('.hoatDong .contentCate .cateClassify').removeClass('activeCate');
+        let result=".hoatDong .contentCate .cateClassify:nth-child("+(valueSubCate+1)+")";
+        $(result).addClass('activeCate');
+        $(".hoatDong .headCate .subCate ul li").removeClass('activeSubCate');
+        $(this).addClass('activeSubCate');
+    });
+    $('.vanHoaXaHoi .headCate .subCate ul li').click(function(){
+        let valueSubCate=$(this).index();
+        $('.vanHoaXaHoi .contentCate .cateClassify').removeClass('activeCate');
+        let result=".vanHoaXaHoi .contentCate .cateClassify:nth-child("+(valueSubCate+1)+")";
+        $(result).addClass('activeCate');
+        $(".vanHoaXaHoi .headCate .subCate ul li").removeClass('activeSubCate');
+        $(this).addClass('activeSubCate');
+    });
+    $('.anNinh .headCate .subCate ul li').click(function(){
+        let valueSubCate=$(this).index();
+        $('.anNinh .contentCate .cateClassify').removeClass('activeCate');
+        let result=".anNinh .contentCate .cateClassify:nth-child("+(valueSubCate+1)+")";
+        $(result).addClass('activeCate');
+        $(".anNinh .headCate .subCate ul li").removeClass('activeSubCate');
+        $(this).addClass('activeSubCate');
     });
 });
